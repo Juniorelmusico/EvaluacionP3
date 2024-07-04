1f40988559b1b3944a44571e95bfa4a960d6e449
Junior Humberto Espin Escobar <junior.espin@udla.edu.ec>
2024-07-04 09:00:39 -0500

    otrito mas

---
+++ SqLite.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using EvaluacionP3.Models;
+
+namespace EvaluacionP3.Data
+{
+    public class PaisDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public PaisDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Pais>().Wait();
+        }
+
+        public Task<List<Pais>> GetPaisesAsync()
+        {
+            return _database.Table<Pais>().ToListAsync();
+        }
+
+        public Task<int> SavePaisAsync(Pais pais)
+        {
+            return _database.InsertAsync(pais);
+        }
+
+        public Task<int> UpdatePaisAsync(Pais pais)
+        {
+            return _database.UpdateAsync(pais);
+        }
+
+        public Task<int> DeletePaisAsync(Pais pais)
+        {
+            return _database.DeleteAsync(pais);
+        }
+    }
+}
---
+++ ViewModels/MainModelView.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using EvaluacionP3.Models;
+using EvaluacionP3.Repository;
+using EvaluacionP3.Data;
+using Microsoft.Maui.Controls;
+
+namespace EvaluacionP3.ViewModels
+{
+    public class MainModelView : BaseViewModel
+    {
+        private readonly RepositoryPaises _repository;
+        private readonly PaisDatabase _database;
+
+        public ObservableCollection<Pais> Paises { get; set; }
+        public ICommand ObtenerPaisesCommand { get; }
+
+        public MainModelView(string dbPath)
+        {
+            _repository = new RepositoryPaises();
+            _database = new PaisDatabase(dbPath);
+            Paises = new ObservableCollection<Pais>();
+            ObtenerPaisesCommand = new Command(async () => await ObtenerPaises());
+        }
+
+        private async Task ObtenerPaises()
+        {
+            var paises = await _repository.DevuelveInfoPaisesAsync();
+            foreach (var pais in paises)
+            {
+                pais.Status = $"{GetInitials("Junior Espin ")}{new Random().Next(1000, 2000)}";
+                await _database.SavePaisAsync(pais);
+                Paises.Add(pais);
+            }
+        }
+
+        private string GetInitials(string name)
+        {
+            return string.Concat(name.Split(' ').Select(x => x[0]));
+        }
+    }
+}
